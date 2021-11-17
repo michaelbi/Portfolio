8184dec7e2d2c280f6d6cab062e8132e779ae399
Michael <10m.b10@gmail.com>
2021-11-17 13:59:03 +0200

    fix 4.5

--- src/Components/Contacts/Contacts.js
+++ src/Components/Contacts/Contacts.js
@@ -39,7 +39,7 @@ function Contact() {
         // }}
       >
         <input
-          name="name"
+          name="email"
           className="s-input"
           type="email"
           placeholder="email"
